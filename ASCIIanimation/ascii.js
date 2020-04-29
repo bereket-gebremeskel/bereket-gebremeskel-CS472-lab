d705d69da1b4aa728e9915dbbdb0a8ca43be16e6
bekijerry <bekigher@gmail.co>
2020-04-28 21:21:09 -0500

    changes

--- ASCIIanimation/ascii.js
+++ ASCIIanimation/ascii.js
@@ -1,9 +1,12 @@
 (function(){
     "use strict";
+    //gloabl variabls
     let speed = 250;
     let selecteAnimType = null;
     let timer =null;
     let animations = null;
+
+    //DOM initializer
     window.onload = function(){
         let turboBtn = document.getElementById("turbo");
         let animStarter = document.getElementById("start");
@@ -12,24 +15,20 @@
         let animationFontSize = document.getElementById("animationFontSize");
        
         animStarter.onclick = animationStarterFun;
-         animStopper.onclick = animationStopperFun;
-         animationSelector.onchange = setAnimationType;
-         animationFontSize.onchange = setAnimationSize;
+        animStopper.onclick = animationStopperFun;
+        animationSelector.onchange = setAnimationType;
+        animationFontSize.onchange = setAnimationSize;
         turboBtn.onclick = speedUpFun;
     };
 
-    function animationStopperFun(){
-        document.getElementById("text-area").value = null;
-        clearInterval(timer );
-        document.getElementById("start").disabled  = false;
-        document.getElementById("stop").disabled  = true;
-    }
 
+    
     function animationStarterFun (){
         document.getElementById("start").disabled  = true;
         document.getElementById("stop").disabled  = false;
-        console.log("selecteAnimType",speed)
+
         const textArea = document.getElementById("text-area");
+
         if(selecteAnimType == 'Juggler'){
             animations = ANIMATIONS["Juggler"].split("=====\n");
             animationloop();
@@ -46,23 +45,35 @@
             animationloop();
         }
     }
+
+    //animation looper
     function animationloop(){
         let count = 0;
         const textArea = document.getElementById("text-area");
         clearInterval(timer );
-            timer   =  setInterval(() => {
+            timer =  setInterval(() => {
                 textArea.value =  animations[count++]; 
                 if(animations.length == count){
                     count = 0;
                 }
             }, speed);
-       
     }
 
+    //animation type setter
     function setAnimationType(){
        selecteAnimType = document.getElementById("animationSelector").value;
-       console.log(selecteAnimType);
     }
+
+    //animation stopper function
+    function animationStopperFun(){
+        document.getElementById("text-area").value = null;
+        clearInterval(timer );
+        document.getElementById("start").disabled  = false;
+        document.getElementById("stop").disabled  = true;
+        const textArea = document.getElementById("text-area");
+        textArea.value = null;
+    }
+
 
 function speedUpFun(){
     let turboBtn = document.getElementById("turbo");
