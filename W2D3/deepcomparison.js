b18d72a74a6265345476e28a39b446440b1629e3
bekijerry <bekigher@gmail.co>
2020-04-29 19:24:00 -0500

    chanegs

--- W2D3/deepcomparison.js
+++ W2D3/deepcomparison.js
@@ -1,59 +1,42 @@
 (function() {
 
-    function deepEqual(left, right) {
-        // console.log(Object.keys(left)); These are just for me to check what
-        // keys are found
-        // console.log(Object.keys(right));
+    
 
+    let obj = {
+        cool : {
+            name : "helo"
+        },
+        id : 2
+    };
+
+    function deepEqual(left, right) {
         if (Object.keys(left).length !== Object.keys(right).length)
             return false;
-        // if unequal number of elements found they are not equal
-
         if (typeof (left) !== typeof (right))
-            return false; // type of checking for comparision
+            return false; 
 
-        if (typeof (left) === "object") {
-            // if type of found to be object we
-            // consider it has key : value so
-            // call for every objects using loop
-            // and recursive function call
-
+        if (typeof (left) === "id") {
             for ( let key in left) {
                 return deepEqual(left[key], right[key]);
             }
         } else {
             if (left === right)
                 return true;
-            // here we reach if the left and right both are
-            // not of object so checking using === for
-            // equality
-            // if equal return true else false
             else
                 return false;
         }
 
     }
-
-    let obj = {
-        here : {
-            is : "an"
-        },
-        object : 2
-    };
     console.log(deepEqual(obj, obj));
-    // → true
     console.log(deepEqual(obj, {
-        here : 1,
-        object : 2
+        cool : 1,
+        id : 2
     }));
-    // → false
     console.log(deepEqual(obj, {
-        here : {
-            is : "an"
+        cool : {
+            name : "helo"
         },
-        object : 2
+        id : 2
     }));
-    // → true
-    console.log("==============================================");
 
 }());
